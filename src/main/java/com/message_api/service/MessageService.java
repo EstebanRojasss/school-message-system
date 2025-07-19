941a9c6bbe1d3994b2be0a1c6904077217126f6d
ESTEBAN <estebanrojasch.2@gmail.com>
2025-07-19 18:41:16 -0300

    feat: Agregar metodo para enviar mensaje en el servicio de mensajería

--- src/main/java/com/message_api/service/MessageService.java
+++ src/main/java/com/message_api/service/MessageService.java
@@ -1,5 +1,10 @@
 package com.message_api.service;
 
+import com.message_api.dto.MessageRequestDto;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,6 +15,31 @@ public class MessageService {
 
     public MessageService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
+    }
+
+    public void enviarMensaje(String remitente, String destinatario, String contenido){
+        MessageRequestDto mensaje = new MessageRequestDto(remitente, destinatario, contenido);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<MessageRequestDto>request = new HttpEntity<>(mensaje, headers);
+
+        String url = "https://localhost:8082/api/mensajes";
+
+        try{
+            ResponseEntity<Void> response = restTemplate.postForEntity(url, request, Void.class);
+
+            if(response.getStatusCode().is2xxSuccessful()){
+                System.out.println("Mensaje enviado con éxito");
+            } else {
+                System.out.println("Error al enviar el mensaje: " + response.getStatusCode());
+            }
+
+        }catch (Exception e){
+            System.out.println("Ocurrio un error al conectar con el sistema de mensajería" + e.getMessage());
+        }
+
     }
 
 }
