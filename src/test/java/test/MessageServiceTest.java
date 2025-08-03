2a142188896d20abb4186f16a3ac08949412fddf
ESTEBAN <estebanrojasch.2@gmail.com>
2025-08-02 21:04:32 -0300

    test

--- src/test/java/test/MessageServiceTest.java
+++ src/test/java/test/MessageServiceTest.java
@@ -1,4 +1,24 @@
 package test;
 
+import com.message_api.service.MessageService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.client.RestTemplate;
+
+@ExtendWith(MockitoExtension.class)
 public class MessageServiceTest {
+
+    @Mock
+    RestTemplate restTemplate;
+
+    @InjectMocks
+    MessageService messageService;
+
+
+    @Test
+    void enviarMensaje_DeberiaEnviarMensaje(){}
+
 }
