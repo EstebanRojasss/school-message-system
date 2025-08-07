d10f6d1e8398de181cb533bea93497d73934df47
ESTEBAN <estebanrojasch.2@gmail.com>
2025-08-06 22:28:35 -0300

    feat: Crear nueva excepcion para Usuario no encontrado

---
+++ src/main/java/com/message_api/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.message_api.exception;
+
+public class UserNotFoundException extends BaseException{
+
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
