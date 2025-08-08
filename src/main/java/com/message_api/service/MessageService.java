f404ca2abd582cbd3b1e37381793725f5e24bed1
ESTEBAN <estebanrojasch.2@gmail.com>
2025-08-08 00:29:30 -0300

    refactor: Eliminar metodo RestTemplate para crear nuevo metodo para la comunicacion con el servidor

--- src/main/java/com/message_api/service/MessageService.java
+++ src/main/java/com/message_api/service/MessageService.java
@@ -1,45 +1,32 @@
 package com.message_api.service;
 
-import com.message_api.dto.MessageRequestDto;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+
+import com.message_api.entities.Mensaje;
+import com.message_api.repository.MessageRepository;
+import com.message_api.repository.UsuarioRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class MessageService {
 
-    private final RestTemplate restTemplate;
+    private final UsuarioRepository usuarioRepository;
+    private final MessageRepository messageRepository;
 
-    public MessageService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+
+    public MessageService(UsuarioRepository usuarioRepository, MessageRepository messageRepository) {
+        this.usuarioRepository = usuarioRepository;
+        this.messageRepository = messageRepository;
     }
 
-    public void enviarMensaje(String remitente, String destinatario, String contenido){
-        MessageRequestDto mensaje = new MessageRequestDto(remitente, destinatario, contenido);
+    public void sendMessage(String remitente, String destinatario, String contenido){
+        Mensaje mensaje = new Mensaje();
+        mensaje.setDestinatario(destinatario);
+        mensaje.setRemitente(remitente);
+        mensaje.setContenido(contenido);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+        messageRepository.save(mensaje);
 
-        HttpEntity<MessageRequestDto>request = new HttpEntity<>(mensaje, headers);
-
-        String url = "https://localhost:8082/api/mensajes";
-
-        try{
-            ResponseEntity<Void> response = restTemplate.postForEntity(url, request, Void.class);
-
-            if(response.getStatusCode().is2xxSuccessful()){
-                System.out.println("Mensaje enviado con éxito");
-            } else {
-                System.out.println("Error al enviar el mensaje: " + response.getStatusCode());
-            }
-
-        }catch (Exception e){
-            System.out.println("Ocurrio un error al conectar con el sistema de mensajería" + e.getMessage());
-        }
-
+        System.out.println("Mensaje guardado correctamente.");
     }
 
 }
